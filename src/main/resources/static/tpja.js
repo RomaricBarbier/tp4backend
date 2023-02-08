33b491ccae425a164c0887d96f5933b2200a27a1
Romaric Barbier <romaric.barbier@etud.univ-jfc.fr>
2023-02-08 12:03:39 +0100

    ajout formulaire

---
+++ src/main/resources/static/tpja.js
@@ -0,0 +1,7 @@
+let cible = document.getElementById("b1");
+cible.addEventListener("click",action1)
+
+function action1(){
+    let code = document.getElementById("code").value;
+    console.log("code")
+}
