31c87f833436d38cb1191725dbebdc2bf000224e
Romaric Barbier <romaric.barbier@etud.univ-jfc.fr>
2023-02-09 15:06:26 +0100

    script et form

--- src/main/resources/static/tpja.js
+++
@@ -1,7 +0,0 @@
-let cible = document.getElementById("b1");
-cible.addEventListener("click",action1)
-
-function action1(){
-    let code = document.getElementById("code").value;
-    console.log("code")
-}
